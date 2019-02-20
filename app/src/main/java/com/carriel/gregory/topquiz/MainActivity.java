30218de65aa882b49ebce05986f1ca8d0cf4c447
G-regory <carriel.g.ca@gmail.com>
2019-02-20 10:12:15 +0100

    create methode controlName() allow to know if user type his name or not for active button play Game. CReate GameActivity.class

--- app/src/main/java/com/carriel/gregory/topquiz/MainActivity.java
+++ app/src/main/java/com/carriel/gregory/topquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.carriel.gregory.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,10 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        controlNameInput();
     }
 
     /**
-     * initialise les views
+     * initialize views
      */
     private void init() {
         mTextViewWelcome=findViewById(R.id.activity_main_msg_welcom_txt);
@@ -28,5 +31,35 @@ public class MainActivity extends AppCompatActivity {
         mButtonPlayGame=findViewById(R.id.activity_main_play_btn);
     }
 
+    /**
+     * Check if user type name
+     * if yes active button
+     * else inactive button
+     */
+    private void controlNameInput(){
+        mButtonPlayGame.setEnabled(false);
+        mEditTextName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                mButtonPlayGame.setEnabled(s.length()>3);
+// if(s.length()>3){
+//                    mButtonPlayGame.setEnabled(true);
+//                }else {
+//                    mButtonPlayGame.setEnabled(false);
+//
+//                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
 }
